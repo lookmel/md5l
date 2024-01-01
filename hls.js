f61e6ee3772a6f611bb88593012e0c7dbbafbf41
lookmel <155245394+lookmel@users.noreply.github.com>
2024-01-02 07:02:11 +0800

    Update hls.js

--- hls.js
+++ hls.js
@@ -5,17 +5,15 @@
 // @description  Custom user script
 // @match        https://haijiao.com/post/details?pid=*
 // @grant        GM_xmlhttpRequest
-// @require      https://lib.baomitu.com/hls.js/0.15.0-alpha.2/hls.min.js?id=1s
-// @require      https://lib.baomitu.com/dplayer/1.25.0/DPlayer.min.js
 // @require      https://raw.githubusercontent.com/lookmel/md5l/main/wejs.js
+// @require      https://lib.baomitu.com/hls.js/0.15.0-alpha.2/hls.min.js?id=1s
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // 创建播放按钮
     let floatButton = document.createElement("button");
-    floatButton.textContent = "点击播放";
+    floatButton.textContent = "点击我！";
     floatButton.style.position = "fixed";
     floatButton.style.left = "0";
     floatButton.style.top = "50%";
@@ -39,40 +37,38 @@
             method: "GET",
             url: url,
             onload: function(response) {
-                let downloadUrl = JSON.parse(response.responseText).downloadUrl;
+              let downloadUrl = JSON.parse(response.responseText).downloadUrl;
 
-                // 创建视频播放器容器
-                let playerDiv = document.createElement("div");
-                playerDiv.style.position = "fixed";
-                playerDiv.style.width = "100%";
-                playerDiv.style.height = "100%";
-                playerDiv.style.top = "0";
-                playerDiv.style.left = "0";
-                playerDiv.style.zIndex = "99999";
-                playerDiv.id = "dplayer";
-                document.body.appendChild(playerDiv);
+              let fullscreenDiv = document.createElement("div");
+              fullscreenDiv.style.position = "fixed";
+              fullscreenDiv.style.width = "100%";
+              fullscreenDiv.style.height = "100%";
+              fullscreenDiv.style.top = "0";
+              fullscreenDiv.style.left = "0";
+              fullscreenDiv.style.zIndex = "99999";
 
-                var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-                if (isMobile) {
-                    // 使用 hls.js 播放 HLS 流
-                    var playerElement = document.createElement("video");
-                    if(Hls.isSupported()) {
-                        var hls = new Hls();
-                        hls.loadSource(downloadUrl);
-                        hls.attachMedia(playerElement);
-                    }
-                    playerDiv.appendChild(playerElement);
-                } else {
-                    // 使用 DPlayer 播放 HLS 流
-                    const dp = new DPlayer({
-                        container: document.getElementById('dplayer'),
-                        screenshot: true,
-                        video: {
-                            url: downloadUrl,
-                            type: 'hls'
-                        }
-                    });
-                }
+              let player = document.createElement("video");
+              player.controls = "controls";
+              player.style.width = "100%";
+              player.style.height = "100%";
+              fullscreenDiv.appendChild(player);
+              document.body.appendChild(fullscreenDiv);
+
+              if (Hls.isSupported()) {
+                  var hls = new Hls();
+                  hls.loadSource(downloadUrl);
+                  hls.attachMedia(player);
+                  hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                     player.play();
+                  });
+               } else if (player.canPlayType('application/vnd.apple.mpegurl')) {
+                  player.src = downloadUrl;
+                  player.addEventListener('loadedmetadata', function() {
+                     player.play();
+                  });
+               }
+
+                console.log("播放器链接: " + player.src);
             }
         });
     });
